06fbffcf6c6ac907805bfbcfd37b1fec975f3343
udiptaroy <udiptaroy@gmail.com>
2014-11-30 12:57:48 -0500

    1130

---
+++ WebContent/resources/js/Transactions.js
@@ -0,0 +1,23 @@
+function mySubmitFunctiontran(){
+	value = $('#frm3').serialize();
+	alert(value);
+    $.ajax({
+    	
+ 	type: "POST",
+ 	  url: "../com.ssdi.studentbudgetcenterserver/rest/usercontroller?"+value,
+     async:true,
+ 	  contentType: "application/json; charset=utf-8",
+      //if received a response from the server
+     success: function(data) {
+     	if(data =="SUCCESS"){
+     		location.reload();
+     				//$("#lblDisplayMsg").text("Data saved successfully");
+     	}
+     	else{
+     		$("#lblDisplayMsg").text(""+data);
+     	}
+     	
+          
+     }
+    });
+}
